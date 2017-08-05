6690c2eb6c935731696177ac065caa198326ab44
Sean N <13018@burnside.school.nz>
2017-08-06 00:32:04 +1200

    00:32 06-08-17
    Need to re-size all svg images on the navigation page.  Also learn how to use a database with flask to store data from html forms.

--- projectPrivacy/routes.py
+++ projectPrivacy/routes.py
@@ -8,20 +8,34 @@ def index():
 
 @app.route("/navigation")
 def navigation():
-	return render_template("/navigation.html")
+	# When x is equal to True it means the user is logged in. 
+	# Havent setup the login pages yet. 
+	# Currently just manually toggling the x value.
+	x = True
+	if x == True:
+		return render_template("/navigation.html", x = True)
+	elif x == False:
+		return render_template("/navigation.html", x = False)
 	
 @app.route("/login")
 def login():
 	return render_template("/login.html")
 	
-@app.route("/help", methods=['GET', 'POST'])
+# For when the forms are working
+# @app.route("/help", methods=['GET', 'POST'])
+
+# For now
+@app.route("/help")
 def help():
-	form = HelpForm()
+	return render_template("/help.html")
 	
-	if request.method == 'POST':
-		return "Success!"
-	elif request.method == 'GET':
-		return render_template("/help.html", form = form)
+# Stuff for forms: Not working	
+	# form = HelpForm()
+	
+	#if request.method == 'POST':
+	#	return "Success!"
+	#elif request.method == 'GET':
+	#	return render_template("/help.html", form = form)
 	
 if __name__ == "__main__":
 	app.run(debug=True)
